05b6ebcdc062ada97c3cac7550e50887b7a56ed4
Vijay <vijayjtuk@hotmail.com>
2013-02-12 16:06:40 +0000

    Bug fixes and other minor enhancements
    Corrected identation error
    Changed implementation of downloadFile to use urlopen in combination
    with shutil.copyfileobj to avoid potential memory issues with large
    fsimage files.

--- getHDFSMetaData.py
+++ getHDFSMetaData.py
@@ -13,7 +13,8 @@ YOUR OWN DEV/TEST ENVIRONMENT BEFORE USING THIS IN PRODUCTION.
 
 import optparse
 import socket
-from urllib2 import Request, urlopen,URLError,HTTPError
+import urllib2
+import shutil
 import sys
 import re
 import os
@@ -32,8 +33,8 @@ import hashlib
     b) Alternatively we could just parse the file name of the last edits file backup
 '''
 
-# Function to check if an IP address is valid (yes we could just use a regex)
 def isValidIPAddress(address):
+    ''' Function to check if an IP address is valid (yes we could just use a regex) '''
     try: socket.inet_aton(address)
     except socket.error: return False
     else: return True
@@ -53,23 +54,32 @@ def isValidPort(port):
         return False
 
 def timestamp(format='%Y-%m-%d-%H-%M-%S'):
+    ''' Returns a timestamp in the format YYY-MM-DD-HH-MM-SS to be used to name our backup files '''
     return datetime.datetime.now().strftime(format)
 
+def dlProgress(block_count, block_size, total_size):
+    total_kb = total_size/1024
+    print "%d kb of %d kb downloaded" %(block_count * (block_size/1024),total_kb )
+
 def downloadFile(URL,fileDest,fileMode='700'):
-    req = Request(URL)
+    ''' Function for downloading / fetching the fsimage and edits files '''
     try:
-        response = urlopen(req)
+        remote_file = urllib2.urlopen(URL)
+         
+        headers = remote_file.info()
+        totalSize = int(headers["Content-Length"])
+        blockSize = 8192
         
-        local_file = open(fileDest, "w" + fileMode)
-        local_file.write(response.read())
-        local_file.close()
-    except HTTPError as e:
+        with open(fileDest, 'wb' + fileMode) as local_file:
+            shutil.copyfileobj(remote_file,local_file,blockSize) 
+	
+    except urllib2.HTTPError as e:
         print "The server couldn\'t fulfil the request."
         print "Error code: ", e.code
-        # It produces ugly output but we print the error page/message so to aid troubleshooting
+        # Althought this produces ugly output but we print the error page/message to aid troubleshooting
         print "Error page follows: ", e.read()
         return False
-    except URLError as e:
+    except urllib2.URLError as e:
         print "We failed to reach a server"
         print "Reason: ", e.reason
         return False
@@ -89,116 +99,119 @@ getHDFSMetaData.py -s namenode.net -p 50070 -r 2 --getedits --startTxId 1 --endT
 
 """
 
-parser = optparse.OptionParser(usage=scriptUsage, version="%prog 0.5")
+if __name__ == "__main__":
+    parser = optparse.OptionParser(usage=scriptUsage, version="%prog 0.5")
 
-parser.add_option('-s', '--server', type='string', dest='namenode_host', help='The hostname of the NameNode')
-parser.add_option('-i', '--ip', type='string', dest='namenode_ip', help='The IP address of the NameNode')
-parser.add_option('-p', '--port', type='int', dest='namenode_port', help='The NameNode Web UI Port (default 50070)', default=50070)
+    parser.add_option('-s', '--server', type='string', dest='namenode_host', help='The hostname of the NameNode')
+    parser.add_option('-i', '--ip', type='string', dest='namenode_ip', help='The IP address of the NameNode')
+    parser.add_option('-p', '--port', type='int', dest='namenode_port', help='The NameNode Web UI Port (default 50070)', default=50070)
 
-parser.add_option('--getimage', action='store_true', help='Retrieve the latest fsimage from the NameNode')
-parser.add_option('--getedits', action='store_true', help='Retrieve the latest edits file from the NameNode')
+    parser.add_option('--getimage', action='store_true', help='Retrieve the latest fsimage from the NameNode')
+    parser.add_option('--getedits', action='store_true', help='Retrieve the latest edits file from the NameNode')
 
-parser.add_option('--startTxId', action='store', help='The starting transaction ID for the edits file. Only required for Apache Hadoop 2.0/CDH4.')
-parser.add_option('--endTxId', action='store', help='The end transaction ID for the edits file. Only required for Apache Hadoop 2.0/CDH4.')
+    parser.add_option('--startTxId', action='store', help='The starting transaction ID for the edits file. Only required for Apache Hadoop 2.0/CDH4.')
+    parser.add_option('--endTxId', action='store', help='The end transaction ID for the edits file. Only required for Apache Hadoop 2.0/CDH4.')
 
-parser.add_option('-r', '--release', type='float', help='The release of Apache Hadoop, 1.0 or 2.0. This affects how the fsimage and edits files are retrieved and is a required parameter.')
+    parser.add_option('-r', '--release', type='float', help='The release of Apache Hadoop, 1.0 or 2.0. This affects how the fsimage and edits files are retrieved and is a required parameter.')
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-#print options
-#print args
-
-
-if (options.namenode_host is None) and (options.namenode_ip is None) or (options.release is None):
-    parser.error("Wrong number of arguements. You must at least specify the NameNode IP address or hostname AND the release AND the getimage or getedits parameters.\n")
-    print scriptUsage
-elif (options.getimage is None) and (options.getedits is None):
-    parser.error("Wrong number of arguements. You must at least specify the NameNode IP address or hostname AND the release AND getimage or getedits parameters.\n")
-    print scriptUsage
+    #print options
+    #print args
 
 
-# Location where backups will be stored
-backupDir = '/backup/hdfs'
-
-if not os.path.exists(backupDir):
-    print "Error: the backup directory %s does not exist. Please create this directory or modify the backupDir variable.\n" % backupDir
-    sys.exit(1)
-if not os.access(backupDir, os.W_OK):
-    print "Error: the backup directory %s is not writable by the user (%s) executing this script.\n" % (backupDir,os.getlogin())
-    sys.exit(1)
-
-
-nnURL = 'http://' 
-hostOrIP = ''
-
-if(options.namenode_port is not None):
-    if not isValidPort(options.namenode_port):
-        print "Invalid port number provided exiting script.\n"
-        sys.exit(1)
-
-if (options.namenode_ip is not None):
-    if isValidIPAddress(options.namenode_ip):
-        #print "Valid IP provided"
-        hostOrIP = options.namenode_ip
-    else:
-        print "Invalid IP address provided, exiting script.\n"
-	sys.exit(1)
-
-if (options.namenode_host is not None):
-    if isValidHostname(options.namenode_host):
-        #print "Valid hostname provided"
-        hostOrIP = options.namenode_host
-    else:
-        print "Invalid IP address provided, exiting script.\n"
-        sys.exit(1)
-
-
-nnURL += hostOrIP + ':' + str(options.namenode_port)
-
-
-if options.getimage:
-    if options.release == 1:
-        nnURL += '/getimage?getimage=1'
-    else:
-        nnURL += '/getimage?getimage=1&txid=latest'
-
-    print "Attempting to retrieve fsimage file from:\n%s\n" % nnURL
-
-    backupFilename = backupDir + '/' + 'fsimage-' + hostOrIP + '-' + timestamp() 
-
-    print "Backup file will be written to: %s\n" % backupFilename
-    if downloadFile(nnURL, backupFilename):
-	print "File successfully downloaded and written to backup directory"
-        fileHash = hashlib.sha1(open(backupFilename,'rb').read()).hexdigest()
-        with open (backupFilename + '.sha1','a') as f: f.write(fileHash)
-        
-        print "Hash (%s) of file written to: %s\n" % (fileHash,backupFilename)
-    else:
-        print "Error: Could not retrieve the fsimage file; exiting script"
-        sys.exit(1)
-
-
-if options.getedits:
-    if (options.startTxId is None) or (options.endTxId is None):
-        print "Error: The --getedits option was specified, this requires the --startTxId and --endTxId are also specified\n"
+    if (options.namenode_host is None) and (options.namenode_ip is None) or (options.release is None):
+        parser.error("Wrong number of arguements. You must at least specify the NameNode IP address or hostname AND the release AND the getimage or getedits parameters.\n")
         print scriptUsage
-    else:
-        if options.release == 1:
-            nnURL += '/getimage?getedit=1'
-        else:
-            nnURL += '/getimage?getedit=1&startTxId=%s&endTxId=%s' % (options.startTxId, options.endTxId) 
-        
-        print "Attempting to retrieve edits file from:\n%s" % nnURL
-         
-        backupFilename = backupDir + '/' + 'edits-' + options.startTxId + '-' + options.endTxId + '-' + hostOrIP + '-' + timestamp()
+    elif (options.getimage is None) and (options.getedits is None):
+        parser.error("Wrong number of arguements. You must at least specify the NameNode IP address or hostname AND the release AND getimage or getedits parameters.\n")
+        print scriptUsage
 
-        print "Backup file will be written to: %s\n" % backupFilename
+
+    # Location where backups will be stored
+    backupDir = '/backup/hdfs'
+
+    if not os.path.exists(backupDir):
+        print "Error: the backup directory %s does not exist. Please create this directory or modify the backupDir variable.\n" % backupDir
+        sys.exit(1)
+    if not os.access(backupDir, os.W_OK):
+        print "Error: the backup directory %s is not writable by the user (%s) executing this script.\n" % (backupDir,os.getlogin())
+        sys.exit(1)
+
+
+    nnURL = 'http://' 
+    hostOrIP = ''
+
+    if(options.namenode_port is not None):
+        if not isValidPort(options.namenode_port):
+            print "Invalid port number provided exiting script.\n"
+            print scriptUsage
+            sys.exit(1)
+
+    if (options.namenode_ip is not None):
+        if isValidIPAddress(options.namenode_ip):
+            #print "Valid IP provided"
+            hostOrIP = options.namenode_ip
+        else:
+            print "Invalid IP address provided, exiting script.\n"
+        sys.exit(1)
+
+    if (options.namenode_host is not None):
+        if isValidHostname(options.namenode_host):
+            #print "Valid hostname provided"
+            hostOrIP = options.namenode_host
+        else:
+            print "Invalid IP address provided, exiting script.\n"
+            sys.exit(1)
+
+
+    nnURL += hostOrIP + ':' + str(options.namenode_port)
+
+
+    if options.getimage:
+        if options.release == 1:
+            nnURL += '/getimage?getimage=1'
+        else:
+            nnURL += '/getimage?getimage=1&txid=latest'
+
+        print "Attempting to retrieve fsimage file from:\n%s\n" % nnURL
+
+        backupFilename = backupDir + '/' + 'fsimage-' + hostOrIP + '-' + timestamp() 
+
+        print "Backup file will be written to: %s.\n" % backupFilename
         if downloadFile(nnURL, backupFilename):
-	    print "File successfully downloaded and written to backup directory"
+            print "File successfully downloaded and written to backup directory."
             fileHash = hashlib.sha1(open(backupFilename,'rb').read()).hexdigest()
             with open (backupFilename + '.sha1','a') as f: f.write(fileHash)
             
             print "Hash (%s) of file written to: %s\n" % (fileHash,backupFilename)
         else:
-            print "Error: Could not retrieve the fsimage file; exiting script"
+            print "Error: Could not retrieve the fsimage file; exiting script."
             sys.exit(1)
+
+
+    if options.getedits:
+        if (options.startTxId is None) or (options.endTxId is None):
+            print "Error: The --getedits option was specified, this requires the --startTxId and --endTxId are also specified.\n"
+            print scriptUsage
+        else:
+            if options.release == 1:
+                nnURL += '/getimage?getedit=1'
+            else:
+                nnURL += '/getimage?getedit=1&startTxId=%s&endTxId=%s' % (options.startTxId, options.endTxId) 
+            
+            print "Attempting to retrieve edits file from:\n%s" % nnURL
+             
+            backupFilename = backupDir + '/' + 'edits-' + options.startTxId + '-' + options.endTxId + '-' + hostOrIP + '-' + timestamp()
+
+            print "Backup file will be written to: %s\n" % backupFilename
+            if downloadFile(nnURL, backupFilename):
+                print "File successfully downloaded and written to backup directory."
+                fileHash = hashlib.sha1(open(backupFilename,'rb').read()).hexdigest()
+                with open (backupFilename + '.sha1','a') as f: f.write(fileHash)
+                
+                print "Hash (%s) of file written to: %s\n" % (fileHash,backupFilename)
+            else:
+                print "Error: Could not retrieve the fsimage file; exiting script."
+                sys.exit(1)
+
